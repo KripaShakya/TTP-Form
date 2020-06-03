955b36fb85a77ca6e1dfa1469d7a27feabfb89e1
MathewDavidov <41017026+MathewDavidov@users.noreply.github.com>
2020-06-03 13:57:13 -0400

    Add Form component and connect it to App.js

---
+++ src/components/Form.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import PropType from "prop-types";
+
+class Form extends Component {
+    
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default Form;
