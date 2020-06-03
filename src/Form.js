80197eb23aba72771b2389e02f29185a96822ac6
Jan Markus Milan <jmarkusm@me.com>
2020-06-03 14:14:52 -0400

    Added base structure for the form edit exercise

---
+++ src/Form.js
@@ -0,0 +1,15 @@
+/*
+Exercise - Form Edit
+• Define a Component that displays a user's first and last name in two div tags on the screen along with an 'edit' button.
+• When the user clicks on the 'edit' button, the user's first and last name are displayed in two input fields along with a 'save' and 'cancel' button.
+  The 'edit' button, and two div tags should not be visible after the user presses 'edit'!
+• The user is then allowed to make any changes they want and either press 'save' or 'cancel'
+• If they press 'save' any changes they made are reflected into the two divs
+• If they press 'cancel' any changes they made are discarded and the divs go back to what they were before pressing 'edit'
+*/
+
+import React, { Component } from "react";
+
+class Form extends Component {}
+
+export default Form;
