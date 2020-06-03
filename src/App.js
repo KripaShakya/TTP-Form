aafe9ba2017ad3604ef4388223886faeeb54fd5c
Jan Markus Milan <jmarkusm@me.com>
2020-06-03 14:55:35 -0400

    Added code for the form edit exercise - could improve by adding classes to each div

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import Form from "./Form";
+import FormEdit from "./FormEdit";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <>
-        <Form />
+        <FormEdit />
       </>
     );
   }
---
+++ src/FormEdit.js
@@ -0,0 +1,79 @@
+/*
+Exercise - Form Edit
+• Define a Component that displays a user's first and last name in two div tags on the screen along with an 'edit' button.
+• When the user clicks on the 'edit' button, the user's first and last name are displayed in two input fields along with a 'save' and 'cancel' button.
+  The 'edit' button, and two div tags should not be visible after the user presses 'edit'!
+• The user is then allowed to make any changes they want and either press 'save' or 'cancel'
+• If they press 'save' any changes they made are reflected into the two divs
+• If they press 'cancel' any changes they made are discarded and the divs go back to what they were before pressing 'edit'
+*/
+
+import React, { Component } from "react";
+
+class FormEdit extends Component {
+  constructor() {
+    super();
+    this.state = {
+      firstName: "John",
+      lastName: "Doe",
+      editState: false,
+    };
+
+    this.baseState = this.state;
+  }
+
+  handleEdit = () => {
+    this.setState({ editState: true });
+  };
+
+  handleSave = () => {
+    this.setState({ editState: false });
+  };
+
+  handleCancel = () => {
+    this.setState({ ...this.baseState, editState: false });
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  render() {
+    let editInfo =
+      this.state.editState === false ? (
+        <div>
+          <div>First Name: {this.state.firstName}</div>
+          <div>Last Name: {this.state.lastName}</div>
+          <br />
+          <button onClick={this.handleEdit}>Edit</button>
+        </div>
+      ) : (
+        <div>
+          <p>Edit First Name: </p>
+          <input
+            type="text"
+            name="firstName"
+            onChange={this.handleChange}
+            placeholder={this.state.firstName}
+          />
+          <p>Edit Last Name: </p>
+          <input
+            type="text"
+            name="lastName"
+            onChange={this.handleChange}
+            placeholder={this.state.lastName}
+          />
+          <br />
+          <br />
+          <button onClick={this.handleSave}>Save</button>
+          <button onClick={this.handleCancel}>Cancel</button>
+        </div>
+      );
+
+    return <div>{editInfo}</div>;
+  }
+}
+
+export default FormEdit;
